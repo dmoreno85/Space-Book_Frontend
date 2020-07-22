1397e8631487b2e4bf2839abe8e08a08364d40bd
David <dmoreno788@gmail.com>
2020-07-23 01:40:11 +0200

    Añadidos validadores de Registro

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import {BrowserRouter, Switch,Route } from 'react-router-dom';
 import Header from './Components/Header/Header.jsx'
 import Home from './Containers/Home/Home.jsx';
 import Profile from './Containers/Profile/Profile.jsx';
+import Register from './Containers/Register/Register.jsx';
 
-
+import './App.scss'
 function App() {
        return (
-    <div className="App">
-    <BrowserRouter>
-     <Header/>
+    <div className="container">
+    <BrowserRouter >
+      <Register/>
+     {/* <Header/>
      <Switch>
        <Route path="/home" component={Home} exact/>
        <Route path="/addPost" exact/>
        <Route path="/profile" component={Profile} exact/>
-     </Switch>
+     </Switch> */}
     </BrowserRouter>
     </div> 
   );
---
+++ src/Containers/Register/Register.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './Register.scss';
+import { NavLink } from 'react-router-dom';
+import { isEmail } from 'validator';
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirm: '',
+            errorName: '',
+            errorEmail: "",
+            errorPassword: "",
+            errorPasswordConfirm: "",
+            nameValidate:false,
+            emailValidate:false,
+            passwordValidate:false,
+            formValidate: false
+        };
+        this.emailInput = React.createRef();
+    };
+    handleSubmit = async (event) => {
+
+        try {
+            event.preventDefault();
+            await this.validateForm();
+            if(this.state.nameValidate ===true & this.state.passwordValidate ===true & this.state.emailValidate ===true) this.setState({formValidate:true})
+            console.log('estado despues de validar ', this.state);
+        } catch (error) {
+        }
+    };
+    validateForm = () => {
+        return new Promise((resolve, reject) => {
+            this.validateEmail();
+            this.validatePassword();
+            this.validateMatchPassword();
+            this.validateName();
+            resolve('form validado')
+            reject('form no validado')
+        })
+    };
+    validateName = () => {
+        const name = this.state.name;
+        if (name.length === 0) this.setState({ errorName: "Introduce un nombre." })
+        else if (name.length > 15) this.setState({ errorName: "Nombre demasiado largo." })
+        else this.setState({ errorName: '',nameValidate:true })
+    };
+    validatePassword = () => {
+        const password = this.state.password;
+        if (password.length === 0) this.setState({ errorPassword: "La contraseña es requerida." })
+        else if (password.length < 8) this.setState({ errorPassword: "La contraseña debe tener mínimo 8 carácteres." })
+        else this.setState({ errorPassword: '',passwordValidate:true })
+    };
+    validateEmail = () => {
+        const email = this.state.email;
+        if (email.length === 0) this.setState({ errorEmail: "El email es requerido." })
+        else if (!isEmail(email)) this.setState({ errorEmail: "Introduzca un email válido" })
+        else this.setState({ errorEmail: '',emailValidate:true })
+    };
+    validateMatchPassword = () => {
+        const password = this.state.password;
+        const passwordConfirm = this.state.passwordConfirm;
+        if (password !== passwordConfirm) this.setState({ errorPasswordConfirm: "Las contraseñas no coinciden." })
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    };
+    render() {
+        return (
+            <form className="registerForm" onSubmit={this.handleSubmit}>
+                <h1>Regístrate</h1>
+                <input type="text"  name="name" value={this.state.name} onChange={this.handleChange} placeholder="Introduce tu nombre" />
+                <div className="errorName"> {this.state.errorName} </div>
+                <input type="email"  name="email" value={this.state.email} onChange={this.handleChange} placeholder="Introduce un Email válido" ref={this.emailInput} />
+                <div className="errorEmail"> {this.state.errorEmail} </div>
+                <input type="password"  name="password" value={this.password} onChange={this.handleChange} placeholder="Crea una contraseña." />
+                <div className="errorPassword"> {this.state.errorPassword} </div>
+                <input type="password"  name="passwordConfirm" value={this.passwordConfirm} onChange={this.handleChange} placeholder="Repita la contraseña." />
+                <div className="errorPasswordConfirm"> {this.state.errorPasswordConfirm} </div>
+                <button type="submit" disabled={this.state.validateForm}>Ok</button>
+                <h4>si ya tienes cuenta <NavLink to="/login">Logueate</NavLink></h4>
+            </form>
+        )
+    };
+};
+
+export default Register;
