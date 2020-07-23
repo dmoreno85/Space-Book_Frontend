3da6fc368effe2d11be8efd4164f70b158362668
David <dmoreno788@gmail.com>
2020-07-23 17:02:13 +0200

    Añadido Login con validadores. cambiado App a clase, para manejar primer renderizado

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import Header from './Components/Header/Header.jsx'
 import Home from './Containers/Home/Home.jsx';
 import Profile from './Containers/Profile/Profile.jsx';
 import Register from './Containers/Register/Register.jsx';
-
 import './App.scss'
-function App() {
-       return (
+import Login from './Containers/Login/Login.jsx';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn:true,
+      isRegistered:false
+    };
+    
+};
+render(){
+  const isLoggedIn = this.state.isLoggedIn;
+  const isRegistered = this.state.isRegistered;
+  return (
     <div className="container">
     <BrowserRouter >
-      <Register/>
-     {/* <Header/>
+    {!isLoggedIn ?
+    <div>
+      {isRegistered? <Login/>: <Register/>
+      }
+      </div> 
+      :
+      <Header/>
+     
+      }
      <Switch>
-       <Route path="/home" component={Home} exact/>
-       <Route path="/addPost" exact/>
-       <Route path="/profile" component={Profile} exact/>
-     </Switch> */}
+        <Route path="/register" component={Register} exact/>
+        <Route path="/login" component={Login} exact/>
+        <Route path="/home" component={Home} exact/>
+        <Route path="/addPost" exact/>
+        <Route path="/profile" component={Profile} exact/>
+      </Switch>
+     
     </BrowserRouter>
     </div> 
-  );
+  )
+}
+     
 }
 
 export default App;
---
+++ src/Containers/Login/Login.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Login.scss';
+import { NavLink } from 'react-router-dom';
+import { isEmail } from 'validator';
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            errorEmail: "",
+            errorPassword: "",
+            emailValidate:false,
+            passwordValidate:false,
+            formValidate: false,
+            errorFormValidate:''
+        };
+        this.emailInput = React.createRef();
+    };
+    handleSubmit = async (event) => {
+
+        try {
+            event.preventDefault();
+            await this.validateForm();
+            if( this.state.passwordValidate ===true & this.state.emailValidate ===true) this.setState({formValidate:true})
+            else this.setState({errorFormValidate:'El email o la contraseña no son válidos.'})
+            console.log('estado despues de validar ', this.state);
+        } catch (error) {
+        }
+    };
+    validateForm = () => {
+        return new Promise((resolve, reject) => {
+            this.validateEmail();
+            this.validatePassword();
+            resolve('form validado')
+            reject('form no validado')
+        })
+    };
+    validatePassword = () => {
+        const password = this.state.password;
+        if (password.length === 0) this.setState({ errorPassword: "La contraseña es requerida." })
+        else if (password.length < 8) this.setState({ errorPassword: "La contraseña debe tener mínimo 8 carácteres." })
+        else this.setState({ errorPassword: '',passwordValidate:true })
+    };
+    validateEmail = () => {
+        const email = this.state.email;
+        if (email.length === 0) this.setState({ errorEmail: "El email es requerido." })
+        else if (!isEmail(email)) this.setState({ errorEmail: "Introduzca un email válido" })
+        else this.setState({ errorEmail: '',emailValidate:true })
+    };
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    };
+    render() {
+        return (
+            <form className="registerForm" onSubmit={this.handleSubmit}>
+                <h1>Inicia sesión</h1>
+             
+                <input type="email"  name="email" value={this.state.email} onChange={this.handleChange} placeholder="Introduce un Email válido" ref={this.emailInput} />
+                <div className="errorEmail"> {this.state.errorEmail} </div>
+                <input type="password"  name="password" value={this.password} onChange={this.handleChange} placeholder="Crea una contraseña." />
+                <div className="errorPassword"> {this.state.errorPassword} </div>
+                <button type="submit" disabled={this.state.validateForm}>Ok</button>
+                <div className="errorFormValidate"> {this.state.errorFormValidate} </div>
+                <h4>si no tienes cuenta, <NavLink to="/Register">Registrate</NavLink></h4>
+            </form>
+        )
+    };
+};
+
+export default Login;
